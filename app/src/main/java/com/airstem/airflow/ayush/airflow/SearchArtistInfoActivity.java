81c793257dd07cd736f1cc4d2354d8e76db5c7de
mcd-50 <ayush.as.shukla@gmail.com>
2017-07-09 22:08:00 +0530

    Fixes

--- app/src/main/java/com/airstem/airflow/ayush/airflow/SearchArtistInfoActivity.java
+++ app/src/main/java/com/airstem/airflow/ayush/airflow/SearchArtistInfoActivity.java
@@ -2,6 +2,8 @@ package com.airstem.airflow.ayush.airflow;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.PagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -10,9 +12,17 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.airstem.airflow.ayush.airflow.adapters.CustomPagerAdapter;
 import com.airstem.airflow.ayush.airflow.adapters.search.AlbumInfoAdapter;
 import com.airstem.airflow.ayush.airflow.behavior.OverlayViewBehavior;
 import com.airstem.airflow.ayush.airflow.behavior.TitleBehavior;
+import com.airstem.airflow.ayush.airflow.fragments.search.ArtistInfoAlbumFragment;
+import com.airstem.airflow.ayush.airflow.fragments.search.ArtistInfoTrackFragment;
+import com.airstem.airflow.ayush.airflow.fragments.search.SearchAlbumFragment;
+import com.airstem.airflow.ayush.airflow.fragments.search.SearchArtistFragment;
+import com.airstem.airflow.ayush.airflow.fragments.search.SearchRadioFragment;
+import com.airstem.airflow.ayush.airflow.fragments.search.SearchTrackFragment;
+import com.airstem.airflow.ayush.airflow.fragments.search.SearchVideoFragment;
 import com.airstem.airflow.ayush.airflow.helpers.ClickListener;
 import com.airstem.airflow.ayush.airflow.model.search.SearchAlbumInfoTrack;
 import com.airstem.airflow.ayush.airflow.model.search.SearchArtistInfoTrack;
@@ -20,18 +30,23 @@ import com.airstem.airflow.ayush.airflow.model.search.SearchArtistInfoTrack;
 import java.util.ArrayList;
 
 import jp.satorufujiwara.scrolling.MaterialScrollingLayout;
+import jp.satorufujiwara.scrolling.MaterialScrollingViewPager;
+import jp.satorufujiwara.scrolling.behavior.ExitUntilCollapsedBehavior;
 import jp.satorufujiwara.scrolling.behavior.ParallaxBehavior;
+import jp.satorufujiwara.scrolling.behavior.ScrollUntilBehavior;
 
 /**
  * Created by mcd-50 on 9/7/17.
  */
 
 public abstract class SearchArtistInfoActivity extends AppCompatActivity implements ClickListener {
+
+    Toolbar toolbar;
     TextView title;
     ImageView image;
     View overlayView;
-    RecyclerView listView;
-    MaterialScrollingLayout scrollingLayout;
+    TabLayout tabLayout;
+    MaterialScrollingViewPager viewPager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -43,22 +58,36 @@ public abstract class SearchArtistInfoActivity extends AppCompatActivity impleme
     }
 
     private void initComponents() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.search_artist_info_page_toolbar);
+        toolbar = (Toolbar) findViewById(R.id.search_artist_info_page_toolbar);
         setSupportActionBar(toolbar);
         if(getSupportActionBar() != null){
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
         title = (TextView) findViewById(R.id.search_artist_info_page_title);
-        image = (ImageView) findViewById(R.id.search_album_info_page_image);
-        overlayView = (View) findViewById(R.id.search_album_info_page_overlay);
-        listView = (RecyclerView) findViewById(R.id.search_album_info_page_list);
-        scrollingLayout = (MaterialScrollingLayout) findViewById(R.id.search_album_info_page_material_scrolling);
+        image = (ImageView) findViewById(R.id.search_artist_info_page_image);
+        overlayView = (View) findViewById(R.id.search_artist_info_page_overlay);
+        tabLayout = (TabLayout) findViewById(R.id.search_artist_info_page_tab);
+        viewPager = (MaterialScrollingViewPager) findViewById(R.id.search_artist_info_page_pager);
 
 
-        scrollingLayout.addBehavior(image, new ParallaxBehavior());
-        scrollingLayout.addBehavior(overlayView, new OverlayViewBehavior(dp(56)));
-        scrollingLayout.addBehavior(title, new TitleBehavior(getResources()));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.app_icon));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.app_icon));
+
+        CustomPagerAdapter adapter = new CustomPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new ArtistInfoTrackFragment());
+        adapter.addFragment(new ArtistInfoAlbumFragment());
+
+        viewPager.setAdapter(adapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager));
+
+        viewPager.addBehavior(image, new ParallaxBehavior());
+        viewPager.addBehavior(tabLayout, new ScrollUntilBehavior(0));
+        viewPager.addBehavior(overlayView, new OverlayViewBehavior(dp(56)));
+        viewPager.addBehavior(toolbar, new ExitUntilCollapsedBehavior(dp(56)));
+        viewPager.addBehavior(title, new TitleBehavior(getResources()));
 
         title.setText(String.valueOf("Loading..."));
 
--- app/src/main/java/com/airstem/airflow/ayush/airflow/fragments/search/ArtistInfoTrackFragment.java
+++ app/src/main/java/com/airstem/airflow/ayush/airflow/fragments/search/ArtistInfoTrackFragment.java
@@ -1,8 +1,42 @@
 package com.airstem.airflow.ayush.airflow.fragments.search;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.airstem.airflow.ayush.airflow.R;
+
 /**
  * Created by mcd-50 on 9/7/17.
  */
 
-public class ArtistInfoTrackFragment {
+public class ArtistInfoTrackFragment extends Fragment {
+
+    TextView textView;
+    RecyclerView listView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.search_artist_info_track_fragment, container, false);
+
+        textView = (TextView) rootView.findViewById(R.id.search_artist_info_track_fragment_empty);
+        listView = (RecyclerView) rootView.findViewById(R.id.search_artist_info_track_fragment_list);
+
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+
+
 }
